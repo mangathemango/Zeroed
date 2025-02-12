c8edf1c445ffcab92060ef56e3098609cf60dc54
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-02-12 23:58:58 +0700

    Add documentation for BaseBullet and BaseEnemy

--- Assets/_Scripts/Base/BaseBullet.cs
+++ Assets/_Scripts/Base/BaseBullet.cs
@@ -1,10 +1,19 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+///* Base bullet class for all bullets in the game
+///! This can't work standalone because there's no general damage dealing system in place yet
+/// </summary>
 public class BaseBullet: MonoBehaviour {
     public bool hit = false;
     public GameObject hitObject;
     public float damage = 0f;
+
+    /// <summary>
+    ///* Setup the bullet with a rigidbody if it doesn't have one
+    /// TODO: Check for more bullet properties, such as colliders, etc.
+    /// </summary>
     public void SetupBullet() {
         if (!GetComponent<Rigidbody>()) {
             gameObject.AddComponent<Rigidbody>();
