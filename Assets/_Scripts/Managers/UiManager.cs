d6522301b027dfd0116e4f2204b51ac66b998a21
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-02-24 06:59:07 +0800

    Fix display bug inside UpdateChamberUI

--- Assets/_Scripts/Managers/UiManager.cs
+++ Assets/_Scripts/Managers/UiManager.cs
@@ -24,42 +24,6 @@ public class UiUpdater: Singleton<UiUpdater> {
         if (playerInventory == null) {
             Debug.LogError("Player not found!");
         }
-        currentWeapon = playerInventory.CurrentWeapon;
-
-        string[] uiElementNames = { "Ammo Count UI", "Chamber UI", "Charge UI", "Fire Mode UI", "Gun Name UI", "Reloading UI" };
-        foreach (string uiElementName in uiElementNames) {
-            GameObject uiObject = GameObject.Find(uiElementName);
-            if (uiObject != null) {
-                switch (uiElementName) {
-                    case "Ammo Count UI":
-                        ammoCountUI = uiObject.GetComponent<TextMeshProUGUI>();
-                        Debug.Log("Ammo Count GameObject found!");
-                        break;
-                    case "Chamber UI":
-                        chamberUI = uiObject.GetComponent<SVGImage>();
-                        Debug.Log("Chamber UI GameObject found!");
-                        break;
-                    case "Charge UI":
-                        chargeUI = uiObject.GetComponent<SVGImage>();
-                        Debug.Log("Charge UI GameObject found!");
-                        break;
-                    case "Fire Mode UI":
-                        fireModeUI = uiObject.GetComponent<TextMeshProUGUI>();
-                        Debug.Log("Fire Mode UI GameObject found!");
-                        break;
-                    case "Gun Name UI":
-                        gunNameUI = uiObject.GetComponent<TextMeshProUGUI>();
-                        Debug.Log("Gun Name UI GameObject found!");
-                        break;
-                    case "Reloading UI":
-                        ReloadingUI = uiObject.GetComponent<RawImage>();
-                        Debug.Log("Reloading UI GameObject found!");
-                        break;
-                }
-            } else {
-                Debug.LogError($"{uiElementName} GameObject not found!");
-            }
-        }
     }
 
     private void Update () {
@@ -89,7 +53,7 @@ public class UiUpdater: Singleton<UiUpdater> {
 
     private float currentReloadRotation = 0;
     private void UpdateChamberUI() {
-        if (currentGun.hasBulletInChamber) {
+        if (!currentGun.hasBulletInChamber) {
             chamberUI.color = new Color(1f, 1f, 1f, 0.5f);
             chargeUI.color = new Color(1f, 1f, 1f, 0.5f);
         }
